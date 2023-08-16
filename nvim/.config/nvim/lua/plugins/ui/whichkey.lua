ca9e77c1b5d251e16981b3707ede88c2970712b1
jarreskng <jarreskng@gmail.com>
2023-08-16 20:12:28 +0600

    [nvim] add whichkey plugin

---
+++ nvim/.config/nvim/lua/plugins/ui/whichkey.lua
@@ -0,0 +1,29 @@
+-- Repo: https://github.com/folke/which-key.nvim
+-- Desc: displays a popup with possible key bindings
+
+return {
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  opts = {
+    icons = {
+      breadcrumb = '⟶'
+    },
+    defaults = {
+      ['g'] = { name = '+goto' },
+      [']'] = { name = '+next' },
+      ['['] = { name = '+prev' },
+      ['<leader>w'] = { name = '+windows' },
+      ['<leader>b'] = { name = '+buffers'},
+      ['<leader>f'] = { name = '+file/find' },
+    }
+  },
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  config = function(_, opts)
+    local wk = require('which-key')
+    wk.setup(opts)
+    wk.register(opts.defaults)
+  end
+}
