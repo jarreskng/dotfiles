aa5b421d7b019951def09517a6e2fe0dbe6ae09c
jarreskng <jarreskng@gmail.com>
2023-08-17 16:29:17 +0600

    [nvim] remove icons deps for lualine

--- nvim/.config/nvim/lua/plugins/ui/lualine.lua
+++ nvim/.config/nvim/lua/plugins/ui/lualine.lua
@@ -4,9 +4,6 @@
 return {
   'nvim-lualine/lualine.nvim',
   event = "VeryLazy",
-  dependencies = { 
-    'nvim-tree/nvim-web-devicons'
-  },
   opts = {
     options = {
       icons_enabled = false,
